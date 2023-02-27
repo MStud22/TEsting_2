8973f4294bdd11ad25a53f23fb87f22ddb1d1ef2
Marco <marcopaglicci22@gmail.com>
2023-02-27 23:55:55 +0100

    Versione 1.0

---
+++ header/Version.h
@@ -0,0 +1,11 @@
+//
+// Created by Marco on 27/02/2023.
+//
+
+#ifndef TESTING_2_VERSION_H
+#define TESTING_2_VERSION_H
+#include <string>
+
+static const std::string VERSION="1.0";
+
+#endif //TESTING_2_VERSION_H
